81f5cac5212ff49da2bbc188e223a9ce2319b4ae
Yoshio Miyamae / Josh <miyamae@yukon-sky.jp>
2023-08-10 21:48:35 +0900

    Fix/ignore not found error (#19)
    * ignore not found error
    
    * increment version

--- src/abs-publisher-plugin.ts
+++ src/abs-publisher-plugin.ts
@@ -4,6 +4,7 @@ import {
   BlobItem,
   BlobServiceClient,
   ContainerClient,
+  RestError,
   StoragePipelineOptions,
   StorageSharedKeyCredential,
 } from '@azure/storage-blob';
@@ -105,16 +106,28 @@ export class AbsPublisherPlugin
     const blockBlobClient = this.containerClient.getBlockBlobClient(
       remoteItem.remotePath
     );
-    const blobDownloadResponse = await blockBlobClient.download();
-    const content = await streamToBuffer(
-      blobDownloadResponse.readableStreamBody
-    );
-    const dirName = path.dirname(item.absPath);
-    await fs.mkdir(dirName, { recursive: true });
-    await fs.writeFile(item.absPath, content);
-    this.logger.verbose(
-      `Downloaded from ${remoteItem.remotePath} to ${item.absPath}`
-    );
+    try {
+      const blobDownloadResponse = await blockBlobClient.download();
+      const content = await streamToBuffer(
+        blobDownloadResponse.readableStreamBody
+      );
+      const dirName = path.dirname(item.absPath);
+      await fs.mkdir(dirName, { recursive: true });
+      await fs.writeFile(item.absPath, content);
+      this.logger.verbose(
+        `Downloaded from ${remoteItem.remotePath} to ${item.absPath}`
+      );
+    } catch (e: unknown) {
+      if (e instanceof RestError) {
+        if (e.statusCode === 404) {
+          this.logger.warn(`Not found ${remoteItem.remotePath}`);
+        } else {
+          throw e;
+        }
+      } else {
+        throw e;
+      }
+    }
     return item;
   }
 
